9311ca15cac18b63778879406b1405384467ceac
imgoodman <jan8611@163.com>
2017-05-17 23:31:20 +0800

    evaluation of linear model and decision tree

--- ch6/regression.py
+++ ch6/regression.py
@@ -1,6 +1,8 @@
 #-*- coding:utf8-*-
 from pyspark import SparkContext
 from pyspark.mllib.regression import LabeledPoint
+from pyspark.mllib.regression import LinearRegressionWithSGD
+from pyspark.mllib.tree import DecisionTree
 import numpy as np
 
 file_path="/usr/bigdata/data/Bike-Sharing-Dataset/hour_noheader.csv"
@@ -58,16 +60,55 @@ def extract_features_dt(record):
     return np.array([float(r) for r in record[2:14]])
 
 data=records.map(lambda r: LabeledPoint(extract_label(r), extract_features(r)))
-first_data=data.first()
-print "raw data: "+str(records.first()[2:])
+#afirst_data=data.first()
+#print "raw data: "+str(records.first()[2:])
 
-print "linear model feature vector: " + str(first_data.features)
-print "length of linear model feature vector: %d" % len(first_data.features)
-print "label of linear model fetaure: %f" % first_data.label
+#print "linear model feature vector: " + str(first_data.features)
+#print "length of linear model feature vector: %d" % len(first_data.features)
+#print "label of linear model fetaure: %f" % first_data.label
 
 data_dt=records.map(lambda r: LabeledPoint(extract_label(r), extract_features_dt(r)))
-first_data_dt=data_dt.first()
-print "decision tree feature vector: " + str(first_data_dt.features)
-print "length of decision tree feature vector: %d" % len(first_data_dt.features)
+#first_data_dt=data_dt.first()
+#print "decision tree feature vector: " + str(first_data_dt.features)
+#print "length of decision tree feature vector: %d" % len(first_data_dt.features)
+
+"""
+train linear model
+"""
+linear_model=LinearRegressionWithSGD.train(data, iterations=10,step=0.1, intercept=False)
+true_vs_predicted=data.map(lambda p: (p.label,linear_model.predict(p.features)))
+#print "linear model predictions:"+str(true_vs_predicted.take(5))
 
 
+"""
+train decision tree
+"""
+dt_model=DecisionTree.trainRegressor(data_dt, {})
+#true_vs_predicted_dt=data_dt.map(lambda p:(p.label,dt_model.predict(p.features)))
+#print "decision tree predictions:"+str(true_vs_predicted_dt.take(5))
+#print "depth of decision tree"+str(dt_model.depth())
+#print "nodes of decision tree"+str(dt_model.numNodes())
+
+"""
+performance of regression model
+actual and prediction
+"""
+def squared_error(actual, pred):
+    return (pred-actual)**2
+
+def abs_error(actual,pred):
+    return np.abs(pred-actual)
+
+def squared_log_error(actual,pred):
+    return (np.log(pred+1) - np.log(actual+1))**2
+
+
+"""
+error of linear model
+"""
+mse=true_vs_predicted.map(lambda (actual,pred): squared_error(actual,pred)).mean()
+mae=true_vs_predicted.map(lambda (actual,pred):abs_error(actual,pred)).mean()
+rmsle=true_vs_predicted.map(lambda (actual,pred):squared_log_error(actual,pred)).mean()
+print "Mean Squared Error of Linear Model: %f" % mse
+print "Mean Absolute Error of Linear Model: %f" % mae
+print "Root Mean Squared Log Error of Linear Model: %f" % rmsle
